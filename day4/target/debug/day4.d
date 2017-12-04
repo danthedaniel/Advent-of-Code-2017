1666835c607b99f1e43f2e674fcb078e6edde72f
teaearlgraycold <supacoofoo@gmail.com>
2017-12-04 02:08:32 -0500

    Revert "Add day 4"
    This reverts commit cb69fa5e484ee56c3bfabc053846eb4c76151d68.

--- day4/target/debug/day4.d
+++
@@ -1,1 +0,0 @@
-/home/daniel/git/Advent-of-Code-2017/day4/target/debug/day4: /home/daniel/git/Advent-of-Code-2017/day4/src/main.rs
--- day4/target/debug/libday4-3bf21a89c85805e1.d
+++
@@ -1,1 +0,0 @@
-/home/daniel/git/Advent-of-Code-2017/day4/target/debug/libday4-3bf21a89c85805e1.rmeta: /home/daniel/git/Advent-of-Code-2017/day4/src/main.rs
