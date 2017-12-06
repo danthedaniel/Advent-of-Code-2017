7e81276d3f9ef2bfeba42b2f317c27afe4f98f2c
teaearlgraycold <supacoofoo@gmail.com>
2017-12-06 14:34:18 -0500

    Add day 6 part 2

--- day6/solution.ex
+++ day6/solution.ex
@@ -1,13 +1,14 @@
 defmodule Day6 do
-    def part1 do
+    def solve do
         list = read_file("input")
-        IO.inspect restack(list), label: "Iterations until a cycle: "
+        IO.inspect restack(list), label: "Iterations / cycle size: "
     end
 
-    def restack(list, set \\ MapSet.new) when is_list(list) do
-        new_set = MapSet.put(set, list)
-        if MapSet.size(new_set) == MapSet.size(set) do
-            MapSet.size(new_set)
+    def restack(list, map \\ Map.new, n \\ 0) when is_list(list) do
+        new_map = Map.put(map, list, n)
+        if Map.size(new_map) == Map.size(map) do
+            cycle_size = n - Map.get(map, list)
+            {Map.size(map), cycle_size}
         else
             {max, index} = list
                 |> Enum.with_index
@@ -15,16 +16,13 @@ defmodule Day6 do
             new_list = List.replace_at(list, index, 0)
             new_list = (index + 1)..(index + max)
                 |> Enum.reduce(new_list, fn(x, acc) -> increment_at(acc, x) end)
-
-            IO.inspect new_list
-
-            restack(new_list, new_set)
+            restack(new_list, new_map, n + 1)
         end
     end
 
     # With wrap-around, increment the value in the list at index n
     def increment_at(list, n) when is_list(list) do
-        index = rem(n, Enum.count(list))
+        index = rem n, Enum.count(list)
         List.update_at(list, index, &(&1 + 1))
     end
 
@@ -38,4 +36,4 @@ defmodule Day6 do
     end
 end
 
-Day6.part1
+Day6.solve
